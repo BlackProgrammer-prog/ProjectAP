b185e415449e476e38fbd7801bad537e0c173aae
Ali.Zandi <ali.zandi2020pc@gmail.com>
2025-05-31 19:49:25 +0330

    Add Folder

---
+++ Frontend/src/pages/dashboard/GeneralApp.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Chats from "./Chats"; // مسیر صحیح فایل Chats را وارد کنید
+
+const GeneralApp = () => {
+  return (
+    <>
+      <Chats />
+    </>
+  );
+};
+
+export default GeneralApp;
