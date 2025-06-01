946273a3dcc84b3a58fa2011ac9af3c39e4828d0
Ali.Zandi <ali.zandi2020pc@gmail.com>
2025-06-01 20:53:24 +0330

    Add contact

--- Frontend/src/pages/dashboard/Chats.js
+++ Frontend/src/pages/dashboard/Chats.js
@@ -1,12 +1,10 @@
-import { alpha, Avatar, Badge, Box, Button, Divider, IconButton, InputBase, Stack, styled, Typography } from '@mui/material';
+import { alpha, Avatar, Badge, Box, Button, Divider, IconButton, InputBase, Stack, styled, Typography, useTheme } from '@mui/material';
 import { ArchiveBox, CircleDashed, MagnifyingGlass, Rows } from 'phosphor-react';
 import React from 'react';
 import { faker } from '@faker-js/faker';
+import { ChatList } from '../../data';
 
-
-
-
-const avatar = faker.image.avatar()
+const avatar = faker.image.avatar();
 
 const Search = styled("div")(({ theme }) => ({
     position: 'relative',
@@ -15,26 +13,25 @@ const Search = styled("div")(({ theme }) => ({
     marginRight: theme.spacing(2),
     marginLeft: 0,
     width: "100%",
-    display: 'flex',  // تغییر به فلت برای قرار گرفتن آیکون و ورودی کنار هم
-    alignItems: 'center'  // برای هم‌راستا کردن آیکون و ورودی
+    display: 'flex',
+    alignItems: 'center',
 }));
 
 const SearchIconWrapper = styled('div')(({ theme }) => ({
-    padding: theme.spacing(0, 1),  // کمی فضا برای آیکون
+    padding: theme.spacing(0, 1),
     display: 'flex',
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
 }));
 
 const StyledInputBase = styled(InputBase)(({ theme }) => ({
     color: 'inherit',
-    padding: theme.spacing(1, 1, 1, 2),  // اضافه کردن پدینگ چپ بیشتر
+    padding: theme.spacing(1, 1, 1, 2),
     width: "100%",
     '& .MuiInputBase-input': {
-        paddingLeft: theme.spacing(2),  // فاصله بیشتر از آیکون
-    }
+        paddingLeft: theme.spacing(2),
+    },
 }));
-
 
 const StyledBadge = styled(Badge)(({ theme }) => ({
     '& .MuiBadge-badge': {
@@ -54,85 +51,64 @@ const StyledBadge = styled(Badge)(({ theme }) => ({
         },
     },
     '@keyframes ripple': {
-        '0%': {
-            transform: 'scale(.8)',
-            opacity: 1,
-        },
-        '100%': {
-            transform: 'scale(2.4)',
-            opacity: 0,
-        },
+        '0%': { transform: 'scale(.8)', opacity: 1 },
+        '100%': { transform: 'scale(2.4)', opacity: 0 },
     },
 }));
 
-const ChatElement = () => {
+const ChatElement = ({ id, name, msg, time, unread, img, online }) => {
+    const Theme = useTheme()
     return (
-        <Box sx={{
-            width: "100%",
-            height: 60,
-            backgroundColor: "#fff",
-            borderRadius: 1
-        }} p={2}>
+        <Box sx={{ width: "100%", height: 60, backgroundColor: Theme.palette.mode === "light" ? "#fff" : Theme.palette.background.paper, borderRadius: 1 }} p={2}>
             <Stack direction={'row'} alignItems={'center'} justifyContent={'space-between'}>
                 <Stack direction={'row'} spacing={2}>
-                    <StyledBadge overlap="circular"
-                        anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
-                        variant="dot">
+                    {online ? (
+                        <StyledBadge overlap="circular" anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }} variant="dot">
+                            <Avatar src={avatar} />
+                        </StyledBadge>
+                    ) : (
                         <Avatar src={avatar} />
-                    </StyledBadge>
+                    )}
                     <Stack spacing={0.3}>
-                        <Typography variant='subtitle2'>
-                            Ali
-                        </Typography>
-                        <Typography variant='caption'>
-                            سلام حالت خوبه؟
-                        </Typography>
+                        <Typography variant='subtitle2'>{name}</Typography>
+                        <Typography variant='caption'>{msg}</Typography>
                     </Stack>
-                    <Stack spacing={2} alignItems={'center'} sx={{
-                        position: 'fixed',
-                        right: 1129
-                    }}>
-                        <Typography sx={{ fontWeight: 600 }} variant='caption'>
-                            9:36
-                        </Typography>
-                        <Badge color='primary' badgeContent={2}>
-
-                        </Badge>
-
-                    </Stack>
+                </Stack>
+                <Stack spacing={2} alignItems={'center'}>
+                    <Typography sx={{ fontWeight: 600 }} variant='caption'>{time}</Typography>
+                    <Badge color='primary' badgeContent={unread}></Badge>
                 </Stack>
             </Stack>
         </Box>
-    )
-}
-
-
+    );
+};
 
 const Chats = () => {
+    const Theme = useTheme()
     return (
         <div>
             <Box
                 sx={{
-                    position: 'fixed',
+                    position: 'absolute',
                     width: 320,
                     height: '100vh',
                     top: 0,
                     left: 100,
-                    backgroundColor: '#F8FAFF',
+                    backgroundColor: Theme.palette.mode === "light" ? '#F8FAFF' : Theme.palette.background.paper,
                     boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.15)',
-                    borderRadius: '8px'
-                }}>
+                    borderRadius: '8px',
+                    // فعال کردن اسکرول برای محتویات چت
+                }}
+            >
                 <Stack sx={{ padding: 3 }}>
                     <Stack direction={'row'} alignItems={'center'} justifyContent={'space-between'}>
-                        <Typography variant='h5'>
-                            Chats
-                        </Typography>
+                        <Typography variant='h5'>Chats</Typography>
                         <IconButton>
                             <CircleDashed />
                         </IconButton>
                     </Stack>
                     <Stack sx={{ width: "100%" }}>
-                        <Search spacing={2}>
+                        <Search>
                             <SearchIconWrapper>
                                 <MagnifyingGlass color='#709CE6' />
                             </SearchIconWrapper>
@@ -140,28 +116,25 @@ const Chats = () => {
                         </Search>
                     </Stack>
                     <Stack spacing={2.5}>
-                        <Stack direction={'row'} alignItems={'center'} spacing={3.3} sx={{
-                            paddingTop: 1,
-                            paddingLeft: 1,
-                            paddingBottom: 0
-                        }}>
+                        <Stack direction={'row'} alignItems={'center'} spacing={3.3} sx={{ paddingTop: 1, paddingLeft: 1 }}>
                             <ArchiveBox size={24} />
-                            <Button>
-                                Archived
-                            </Button>
+                            <Button>Archived</Button>
                         </Stack>
                         <Divider />
                     </Stack>
-                    <Stack direction={"column"}>
+                    <Stack direction={"column"} sx={{ maxHeight: 'calc(100vh - 200px)', overflowY: 'scroll' }}>
                         <Stack spacing={2.4}>
-                            <Typography variant='subtitle2' sx={{
-                                color: '#676767'
-                            }}>
-                                Pinned
-                            </Typography>
-                            <ChatElement />
+                            <Typography variant='subtitle2' sx={{ color: '#676767' }}>Pinned</Typography>
+                            {ChatList.filter((el) => el.pinned).map((el) => (
+                                <ChatElement {...el} />
+                            ))}
                         </Stack>
-
+                        <Stack spacing={2.4}>
+                            <Typography variant='subtitle2' sx={{ color: '#676767' }}>All Chat</Typography>
+                            {ChatList.filter((el) => !el.pinned).map((el) => (
+                                <ChatElement {...el} />
+                            ))}
+                        </Stack>
                     </Stack>
                 </Stack>
             </Box>
