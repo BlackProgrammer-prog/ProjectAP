e60cd21398c9ac0bb8bcc82df746734dc0e9f076
Ali.Zandi <ali.zandi2020pc@gmail.com>
2025-06-04 14:52:23 +0330

    Update GeneralApp.js

--- Frontend/src/pages/dashboard/GeneralApp.js
+++ Frontend/src/pages/dashboard/GeneralApp.js
@@ -1,25 +1,65 @@
-import React, { lazy, Suspense } from "react";
-import Chats from "./Chats";
-import { Box, Stack } from "@mui/material";
-import Conversation from "../../components/Conversation";
-import { useTheme } from "@emotion/react";
+// import React, { lazy, Suspense } from "react";
+// import Chats from "./Chats";
+// import { Box, Stack } from "@mui/material";
+// import Conversation from "../../components/Conversation";
+// import { useTheme } from "@emotion/react";
 
-const Cat = lazy(() => import("../../components/Cat"));
+// const Cat = lazy(() => import("../../components/Cat"));
+
+// const GeneralApp = () => {
+//   const theme = useTheme()
+//   return (
+//     <Stack direction={"row"} sx={{ width: 'auto' }}>
+//       <Chats />
+//       <Box sx={{ height: '100%', width: "calc(100vw - 420px)",
+//          backgroundColor: theme.palette.mode === 'light' ? '#2C3E50' : theme.palette.background.pape, 
+//          position: "relative", top: 0, left: 410 }}>
+//         <Conversation />
+//       </Box>
+//     </Stack>
+//   );
+// };
+
+// export default GeneralApp;
+
+// .........................................MAIN
+
+// "use client"
+
+import { lazy } from "react"
+import Chats from "./Chats"
+import { Box, Stack } from "@mui/material"
+import Conversation from "../../components/Conversation"
+import { useTheme } from "@mui/material/styles"
+
+const Cat = lazy(() => import("../../components/Cat"))
 
 const GeneralApp = () => {
   const theme = useTheme()
   return (
-    <Stack direction={"row"} sx={{ width: 'auto' }}>
+    <Stack direction={"row"} >
+      {/* Sidebar چت‌ها */}
       <Chats />
-      <Box sx={{
-        height: '100%', width: "calc(100vw - 420px)",
-        backgroundColor: theme.palette.mode === 'light' ? '#fff' : theme.palette.background.default,
-        position: "relative", top: 0, left: 410
-      }}>
+
+      {/* ناحیه اصلی مکالمه */}
+      <Box
+        sx={{
+          position: 'fixed',
+          top: 30,
+          left: 420,
+          height: "100vh",
+          width: "calc(100vw - 420px)",
+          backgroundColor: theme.palette.mode === "light" ? "#F0F4FA" : theme.palette.background.paper,
+
+        }}
+      >
         <Conversation />
       </Box>
     </Stack>
-  );
-};
+  )
+}
 
-export default GeneralApp;
+export default GeneralApp
+
+
+// ...........................................................................................
