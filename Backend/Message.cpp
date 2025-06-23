1827ab6776823866a64e248a7d2238e1bce4e4fe
Mohadese Javan <jvnm6006@gmail.com>
2025-06-24 01:02:36 +0330

    ✨ feat: Create Message.cpp
    added basic message struct with JSON support and UUID generation.

---
+++ Backend/Message.cpp
@@ -0,0 +1,22 @@
+#include "Message.h"
+#include <boost/uuid/uuid.h>
+#include <boost/uuid/uuid_generators.h>
+#include <boost/uuid/uuid_io.h>
+#include <chrono>
+
+std::string Message::generateUUID() {
+    static boost::uuids::random_generator gen;
+    return boost::uuids::to_string(gen());
+}
+
+Message::Message(const std::string& sender,
+                 const std::string& receiver,
+                 MessageType msg_type,
+                 const std::string& msg_content)
+    : id(generateUUID()),
+      sender_id(sender),
+      receiver_id(receiver),
+      type(msg_type),
+      content(msg_content),
+      timestamp(std::chrono::system_clock::to_time_t(std::chrono::system_clock::now()))
+{}
