e810d1004ca093ff6b0be02717358ec8553fcd80
Fatavana <fatavana06@gmail.com>
2025-08-07 17:07:21 +0300

    Define empty functions in ProfileManager
    Defined empty function bodies for constructor, loadProfile, saveProfile, and clearProfile in the .cpp file.

--- Backend/ProfileManager.cpp
+++ Backend/ProfileManager.cpp
@@ -1,2 +1,9 @@
-
 #include "ProfileManager.h"
+
+ProfileManager::ProfileManager() {}
+
+void ProfileManager::loadProfile(const std::string& email) {}
+
+void ProfileManager::saveProfile() {}
+
+void ProfileManager::clearProfile() {}
