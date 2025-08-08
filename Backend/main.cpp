91776cf12109b41dc3f86956538501933fd99bc2
Parham Azizi <aziziparham2020@gmail.com>
2025-08-09 01:00:06 +0330

    add Database Static 🗃️

--- Backend/main.cpp
+++ Backend/main.cpp
@@ -21,7 +21,7 @@
 
 int main() {
     // تنظیمات اولیه
-    const std::string DB_PATH = "app_database.db";
+    const std::string DB_PATH = "C:/Users/HOME/Desktop/ProjectAP/ProjectAP/Database/app_database.db";
     const int WS_PORT = 8081;
     const int HTTP_PORT = 8080;
     const std::string JWT_SECRET = "your_strong_jwt_secret_here";
