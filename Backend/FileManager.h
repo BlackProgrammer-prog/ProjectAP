b851f2115779f22c51137a872a7129145ddc1a84
Mohadese Javan <jvnm6006@gmail.com>
2025-08-12 15:57:57 +0300

    Add FileManager class definition✨️

--- Backend/FileManager.h
+++ Backend/FileManager.h
@@ -1,14 +1,20 @@
 //
-// Created by TUF_Fx507VV on 12/8/2025.
-//
+// Created by TUF_Fx507VV on 12/8/2025. 
+// 
 
-#ifndef FileManager_FileManager_H
-#define FileManager_FileManager_H
 
+#ifndef BACKEND_FILEMANAGER_H
+#define BACKEND_FILEMANAGER_H
+
+#include <string>
+#include <vector>
 
 class FileManager {
+public:
+    static std::string saveBase64File(const std::string& base64Data, const std::string& directory, const std::string& preferredName);
 
+private:
+    static std::vector<unsigned char> base64Decode(const std::string& encodedString);
 };
 
-
-#endif //FileManager_FileManager_H
+#endif //BACKEND_FILEMANAGER_H
