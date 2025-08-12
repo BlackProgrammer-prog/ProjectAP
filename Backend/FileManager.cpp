0946c9c4e45f25beea5e6c57d618d813250a0a3d
Mohadese Javan <jvnm6006@gmail.com>
2025-08-12 15:47:44 +0300

    Create empty FileManager source files📑

---
+++ Backend/FileManager.cpp
@@ -0,0 +1,5 @@
+//
+// Created by TUF_Fx507VV on 12/8/2025.
+//
+
+#include "FileManager.h"
---
+++ Backend/FileManager.h
@@ -0,0 +1,14 @@
+//
+// Created by TUF_Fx507VV on 12/8/2025.
+//
+
+#ifndef FileManager_FileManager_H
+#define FileManager_FileManager_H
+
+
+class FileManager {
+
+};
+
+
+#endif //FileManager_FileManager_H
