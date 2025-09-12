6ae674a82a5efd688e1c3c2e0e5f28b21ac05d53
Ali.Zandi <ali.zandi2020pc@gmail.com>
2025-09-12 13:08:30 +0330

    Update FileManager.h

--- Backend/FileManager.h
+++ Backend/FileManager.h
@@ -1,4 +1,24 @@
 
+// //
+// // Created by afraa on 8/8/2025.
+// //
+
+// #ifndef BACKEND_FILEMANAGER_H
+// #define BACKEND_FILEMANAGER_H
+
+// #include <string>
+// #include <vector>
+
+// class FileManager {
+// public:
+//     static std::string saveBase64File(const std::string& base64Data, const std::string& directory, const std::string& preferredName);
+
+// private:
+//     static std::vector<unsigned char> base64Decode(const std::string& encodedString);
+// };
+
+// #endif //BACKEND_FILEMANAGER_H
+// -------------------------------------------------------ali
 //
 // Created by afraa on 8/8/2025.
 //
@@ -8,13 +28,18 @@
 
 #include <string>
 #include <vector>
+#include <cstdint>
+#include <string_view>
 
-class FileManager {
+class FileManager
+{
 public:
-    static std::string saveBase64File(const std::string& base64Data, const std::string& directory, const std::string& preferredName);
+    [[nodiscard]] static std::string saveBase64File(std::string_view base64Data,
+                                                    std::string_view directory,
+                                                    std::string_view preferredName);
 
 private:
-    static std::vector<unsigned char> base64Decode(const std::string& encodedString);
+    static std::vector<std::uint8_t> base64Decode(std::string_view encodedString);
 };
 
-#endif //BACKEND_FILEMANAGER_H
+#endif // BACKEND_FILEMANAGER_H
